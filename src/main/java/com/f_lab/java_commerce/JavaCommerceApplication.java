eb1c6b009d3494bc865c1ba946ff435d5932a959
hee-c <ways2kim@gmail.com>
2024-08-26 21:19:57 +0900

    chore: spring boot 초기 설정 추가

---
+++ src/main/java/com/f_lab/java_commerce/JavaCommerceApplication.java
@@ -0,0 +1,13 @@
+package com.f_lab.java_commerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaCommerceApplication.class, args);
+	}
+
+}
