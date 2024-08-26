187b3d3ab1e80cee3e19a8a3b27c5515d383361e
hee-c <ways2kim@gmail.com>
2024-08-26 21:19:57 +0900

    chore: spring boot 초기 설정 추가

---
+++ src/test/java/com/f_lab/java_commerce/JavaCommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.f_lab.java_commerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaCommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
