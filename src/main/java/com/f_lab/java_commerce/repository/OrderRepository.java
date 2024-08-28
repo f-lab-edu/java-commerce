a009e34c1e2a9fc61e633b181fc9b8a84d656b30
hee-c <ways2kim@gmail.com>
2024-08-28 20:45:54 +0900

    feature: Order repository 추가

---
+++ src/main/java/com/f_lab/java_commerce/repository/OrderRepository.java
@@ -0,0 +1,17 @@
+package com.f_lab.java_commerce.repository;
+
+import com.f_lab.java_commerce.domain.Order;
+import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepository {
+
+    private final EntityManager em;
+
+    public void save(Order order) {
+        em.persist(order);
+    }
+}
