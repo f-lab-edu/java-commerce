02fe79c5319b98e3d91976cfb71f0d7ce7992f3d
hee-c <ways2kim@gmail.com>
2024-09-02 20:01:04 +0900

    feature: PgMockApplication 설정

---
+++ apps/pg-mock/src/main/java/com/f_lab/pg_mock/PgMockApplication.java
@@ -0,0 +1,12 @@
+package com.f_lab.pg_mock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PgMockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PgMockApplication.class, args);
+    }
+}
